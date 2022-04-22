f96604349f304059aeb5b615d7f9f33388bac0d3
pyatikin <86558209+pyatikin@users.noreply.github.com>
2022-04-22 08:32:02 +0300

    Add files via upload
    1

---
+++ read_file.c
@@ -0,0 +1,116 @@
+#include "fdf.h"
+
+int	chars(int fd, int i, char *argc)
+{
+	char	b;
+
+	fd = open(argc, O_RDONLY);
+	if (fd < 0)
+	{
+		i = write(1, "error\n", 6);
+		exit(1);
+	}
+	i = 0;
+	while (read(fd, &b, 1))
+		i++;
+	i++;
+	close(fd);
+	return (i);
+}
+
+char	*buffer(int fd, int i, char *argc, char *buf)
+{	
+	char	b;
+
+	i = 0;
+	fd = open(argc, O_RDONLY);
+	while (read(fd, &b, 1))
+		buf[i++] = b;
+	buf[i] = '\0';
+	close(fd);
+	return (buf);
+}
+
+void	map(char *buf, int i, int fd, t_fdf *field)
+{
+	i = 0;
+	fd = fd + 1;
+	while (buf[i] && buf[i] != '\n')
+	{
+		i++;
+		if (buf[i] == ' ')
+		{
+			while (buf[i + 1] == ' ')
+				i++;
+			field->width++;
+		}
+	}
+	i = 0;
+	while (buf[i + 1])
+	{
+		if (buf[i] == '\n')
+		{
+			field->hight++;
+			fd = 1;
+		}
+		i++;
+	}
+}
+
+int	**full_map(char *buf, int fd, t_fdf field)
+{
+	int	i;
+	int	count;
+
+	i = 0;
+	fd = 0;
+	count = 0;
+	while (field.hight != i)
+	{
+		del_pointer(&field, i);
+		field.matrix[i][count] = ft_atoi(&buf[fd]);
+		count++;
+		while (buf[fd] != '\n' && buf[fd])
+		{
+			if (buf[fd] == ' ')
+			{
+				while (buf[fd + 1] == ' ')
+					fd++;
+				field.matrix[i][count] = ft_atoi(&buf[fd + 1]);
+				count++;
+			}
+			fd++;
+		}
+		counter(&i, &count, &fd);
+	}
+	return (field.matrix);
+}
+
+t_fdf	main_reader(char *argc)
+{
+	int		i;
+	char	*buf;
+	t_fdf	field;
+
+	i = 0;
+	field.width = 1;
+	field.hight = 1;
+	buf = (char *)malloc(sizeof(char) * chars(0, i, argc));
+	if (!buf)
+	{
+		free(buf);
+		exit(1);
+	}
+	buf = buffer(0, i, argc, buf);
+	map (buf, i, 0, &field);
+	field.matrix = (int **)malloc(sizeof(int *) * field.hight);
+	if (!field.matrix)
+	{
+		free(field.matrix);
+		free(buf);
+		exit(1);
+	}
+	field.matrix = full_map(buf, 0, field);
+	free(buf);
+	return (field);
+}
